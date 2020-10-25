0e8338777c00c756539c7b9566ea6c34c44fffff
Olivier MICHAUD <o.michaud@hsrv.fr>
2020-10-25 23:13:39 +0100

    Minimalist assets deployment

---
+++ assets/routes.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import DashboardLayout from './layouts/DashboardLayout';
+import MainLayout from './layouts/MainLayout';
+import DashboardView from './views/DashboardView';
+import NotFoundView from './views/NotFoundView';
+import SettingsView from './views/SettingsView';
+
+const routes = [
+  {
+    path: 'app',
+    element: <DashboardLayout />,
+    children: [
+      { path: 'dashboard', element: <DashboardView /> },
+      { path: 'settings', element: <SettingsView /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '404', element: <NotFoundView /> },
+      { path: '/', element: <Navigate to="/app/dashboard" /> },
+      { path: '*', element: <Navigate to="/404" /> }
+    ]
+  }
+];
+
+export default routes;
