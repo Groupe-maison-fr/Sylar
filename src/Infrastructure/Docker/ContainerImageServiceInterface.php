e7566d198348d98a72c617cdf21e38bf6b192e33
Olivier MICHAUD <o.michaud@hsrv.fr>
2020-10-25 23:29:16 +0100

    Add a service to manage and download local docker images

---
+++ src/Infrastructure/Docker/ContainerImageServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Docker;
+
+interface ContainerImageServiceInterface
+{
+    public function imageExists(string $imageName): bool;
+
+    public function pullImage(string $imageName);
+}
