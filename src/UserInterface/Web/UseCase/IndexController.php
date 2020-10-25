96e0b611b94e756abb8063be12d00f7fe3dcbdeb
Olivier MICHAUD <o.michaud@hsrv.fr>
2020-10-25 23:36:39 +0100

    Add a controller to host web application

---
+++ src/UserInterface/Web/UseCase/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UserInterface\Web\UseCase;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class IndexController
+{
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    public function __construct(Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    public function index(): Response
+    {
+        return new Response($this->environment->render('@UseCase/index.html.twig'));
+    }
+}
