eb7ed1eee1b4478a6323cbf3ac9dac9a1950bd89
Olivier MICHAUD <o.michaud@hsrv.fr>
2020-10-25 23:25:32 +0100

    Add a container parameter system management

---
+++ src/Infrastructure/Docker/ContainerParameter/LabelFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Docker\ContainerParameter;
+
+use App\Core\ServiceCloner\Configuration\Object\Label;
+
+final class LabelFactory implements LabelFactoryInterface
+{
+    /**
+     * @var ConfigurationExpressionGeneratorInterface
+     */
+    private $configurationExpressionGenerator;
+
+    public function __construct(
+        ConfigurationExpressionGeneratorInterface $configurationExpressionGenerator
+    ) {
+        $this->configurationExpressionGenerator = $configurationExpressionGenerator;
+    }
+
+    public function createFromConfiguration(ContainerParameterDTO $containerParameter, Label $label): array
+    {
+        return [
+            $this->configurationExpressionGenerator->generate($containerParameter, $label->getName()),
+            $this->configurationExpressionGenerator->generate($containerParameter, $label->getValue()),
+        ];
+    }
+}
