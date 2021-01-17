640426f4b90e0947117044ea5dc70ff1608570d3
Olivier MICHAUD <o.michaud@hsrv.fr>
2021-01-17 23:52:03 +0100

    Add a byte formatter and parser

---
+++ src/Infrastructure/Filesystem/BytesFormatConvertor.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Filesystem;
+
+final class BytesFormatConvertor implements BytesFormatConvertorInterface
+{
+    private const units = [
+        'B' => 1,
+        'K' => 1024,
+        'M' => 1024 * 1024,
+        'G' => 1024 * 1024 * 1024,
+        'T' => 1024 * 1024 * 1024 * 1024,
+    ];
+
+    public function parse(string $formattedSize): int
+    {
+        $formattedSize = trim($formattedSize);
+        if ($formattedSize === '-') {
+            return 0;
+        }
+        if ($formattedSize === '') {
+            return 0;
+        }
+        if (!preg_match('!^(\d+)(\.\d+){0,1}([BKMGT])$!', $formattedSize, $matches)) {
+            throw new UnitFormatException($formattedSize);
+        }
+        $value = (int) $matches[1];
+        $precision = $matches[2];
+        $unitSize = self::units[$matches[3]];
+        if (empty($precision)) {
+            return $value * $unitSize;
+        }
+        return $value * $unitSize + ((int ) (floatval($precision) * $unitSize));
+    }
+
+    public function format($bytes, $precision = 3): string
+    {
+        $base = log($bytes, 1024);
+
+        return round(pow(1024, $base - floor($base)), $precision) . array_keys(self::units)[floor($base)];
+    }
+
+}
---
+++ tests/Infrastructure/Filesystem/SizeFormatConvertorTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Infrastructure\Filesystem;
+
+use App\Infrastructure\Filesystem\BytesFormatConvertorInterface;
+use App\Infrastructure\Filesystem\UnitFormatException;
+use Tests\AbstractIntegrationTest;
+
+/**
+ * @internal
+ */
+final class SizeFormatConvertorTest extends AbstractIntegrationTest
+{
+    private BytesFormatConvertorInterface $sizeFormatConvertor;
+
+    /**
+     * @test
+     */
+    public function it_should_parse_empty_data(): void
+    {
+        self::assertSame(0, $this->sizeFormatConvertor->parse(' '));
+        self::assertSame(0, $this->sizeFormatConvertor->parse(''));
+        self::assertSame(0, $this->sizeFormatConvertor->parse('-'));
+        self::assertSame(0, $this->sizeFormatConvertor->parse('0B'));
+    }
+
+    /**
+     * @test
+     * @dataProvider valid_bytes_data
+     */
+    public function it_should_parse_valid_string(string $formattedString, int $size): void
+    {
+        self::assertSame($size, $this->sizeFormatConvertor->parse($formattedString));
+    }
+
+    /**
+     * @test
+     * @dataProvider valid_bytes_data
+     */
+    public function it_should_format_bytes(string $formattedString, int $size): void
+    {
+        self::assertSame($formattedString, $this->sizeFormatConvertor->format($size));
+    }
+
+    public function valid_bytes_data(): array
+    {
+        return [
+            ['1B', 1],
+            ['1023B', 1023],
+            ['1K', 1024],
+            ['1023K', 1024 * 1023],
+            ['1M', 1024 * 1024],
+            ['1023M', 1024 * 1024 * 1023],
+            ['1G', 1024 * 1024 * 1024],
+            ['1023G', 1024 * 1024 * 1024 * 1023],
+            ['1T', 1024 * 1024 * 1024 * 1024],
+            ['1023T', 1024 * 1024 * 1024 * 1024 * 1023],
+            ['1.222K', 1251],
+            ['1.21K', 1239],
+            ['1.211K', 1240],
+            ['1.475M', 1546649],
+            ['1.2M', 1258291],
+            ['139.7M', 146486067],
+            ['1.3G', 1395864371],
+            ['724.1G', 777496454758],
+            ['1.1T', 1209462790553],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider it_should_not_parse_unformatted_string_data
+     */
+    public function it_should_not_parse_unformatted_string(string $badString): void
+    {
+        self::expectException(UnitFormatException::class);
+        self::expectExceptionMessage(sprintf('Size "%s" could not be parsed', trim($badString)));
+        $this->sizeFormatConvertor->parse($badString);
+    }
+
+    public function it_should_not_parse_unformatted_string_data(): array
+    {
+        return [
+            [' A12B'],
+            ['123MAB'],
+            ['B'],
+            ['K12'],
+            [' 12 B'],
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->sizeFormatConvertor = $this->getService(BytesFormatConvertorInterface::class);
+    }
+}
