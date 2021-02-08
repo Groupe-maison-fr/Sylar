6100de48e07bcaf7e9a24f3bba7ce8d3b97e198e
Olivier MICHAUD <o.michaud@hsrv.fr>
2021-02-08 17:28:28 +0100

    Isolate docker tests

--- tests/Core/ServiceCloneService/ServiceCloneServiceIntegrationTest.php
+++ tests/Core/ServiceCloneService/ServiceCloneServiceIntegrationTest.php
@@ -64,15 +64,15 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
 
         $this->process = $this->getService(SudoProcess::class);
         $this->testRoot = '/tmp';
+        $this->cleanExistingDockers();
 
         echo $this->process->mayRun('zfs', 'destroy', '-Rf', 'testpool');
-        echo $this->process->mayRun('zpool', 'destroy', '-Rf', 'testpool');
+        echo $this->process->mayRun('zpool', 'destroy', '-f', 'testpool');
         echo $this->process->mayRun('rm', '-f', $this->testRoot . '/testdisk');
         echo $this->process->run('fallocate', '-l', '2G', $this->testRoot . '/testdisk');
         echo $this->process->run('zpool', 'create', 'testpool', $this->testRoot . '/testdisk');
 
         $this->resetBufferedLoggerHandler();
-        $this->cleanExistingDockers();
     }
 
     private function setDependentServices(string $testConfigurationName): void
@@ -83,6 +83,7 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
         $this->containerCreationService = $this->getService(ContainerCreationServiceInterface::class);
         $this->containerExecService = $this->getService(ContainerExecServiceInterface::class);
         $this->containerFinderService = $this->getService(ContainerFinderServiceInterface::class);
+        $this->containerStopService = $this->getService(ContainerStopServiceInterface::class);
     }
 
     protected function tearDown(): void
@@ -90,7 +91,7 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
         $this->resetBufferedLoggerHandler();
         $this->cleanExistingDockers();
         $this->process->run('zfs', 'destroy', '-Rf', 'testpool');
-        $this->process->run('zpool', 'destroy', 'testpool');
+        $this->process->run('zpool', 'destroy', '-f', 'testpool');
         $this->process->run('rm', '-f', $this->testRoot . '/testdisk');
     }
 
@@ -115,7 +116,7 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
     public function it_should_load_configuration_service_with_specific_path(): void
     {
         $this->setDependentServices('specific_configuration');
-        $config = $this->configurationService->getConfiguration()->getServiceByName('mysql');
+        $config = $this->configurationService->getConfiguration()->getServiceByName('mysql_specific_configuration');
         self::assertSame('ABCDEFGH', $config->getLabels()->get(1)->getValue());
     }
 
@@ -135,11 +136,11 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
     public function it_should_start_mysql_master_and_can_query_on_it(): void
     {
         $this->setDependentServices('start_master');
-        $this->serviceCloneService->start('mysql', 'master', 0);
-        $tableNames = explode(PHP_EOL, $this->containerExecMysql('mysql', 'select TABLE_NAME from information_schema.TABLES'));
-        $slowQueryVariables = explode(PHP_EOL, $this->containerExecMysql('mysql', 'show variables like \'slow_query_log_file\''));
+        $this->serviceCloneService->start('mysql_start_master', 'master', 0);
+        $tableNames = explode(PHP_EOL, $this->containerExecMysql('mysql-start-master', 'select TABLE_NAME from information_schema.TABLES'));
+        $slowQueryVariables = explode(PHP_EOL, $this->containerExecMysql('mysql-start-master', 'show variables like \'slow_query_log_file\''));
 
-        self::assertSame('root_password', $this->containerExecShell('mysql', 'echo ${MYSQL_ROOT_PASSWORD}'));
+        self::assertSame('root_password', $this->containerExecShell('mysql-start-master', 'echo ${MYSQL_ROOT_PASSWORD}'));
 
         self::assertSame('TABLE_NAME', current($tableNames));
         self::assertContains('SESSION_VARIABLES', $tableNames);
@@ -152,34 +153,34 @@ final class ServiceCloneServiceIntegrationTest extends AbstractIntegrationTest
     public function it_should_start_master_and_clones(): void
     {
         $this->setDependentServices('start_master_clones');
-        $this->serviceCloneService->start('mysql', 'master', 0);
-        $this->containerExecMysql('mysql', 'create database testdb;');
-        $this->containerExecMysql('mysql', <<<EOS
+        $this->serviceCloneService->start('mysql_start_master_clones', 'master', 0);
+        $this->containerExecMysql('mysql-start-master-clones', 'create database testdb;');
+        $this->containerExecMysql('mysql-start-master-clones', <<<EOS
             create table testdb.test (
                 idx INT(11) NOT NULL AUTO_INCREMENT, 
                 val VARCHAR(30) NOT NULL,
                 UNIQUE KEY ix_idx (idx) USING BTREE
             );
 EOS);
-        $this->containerExecMysql('mysql', <<<EOS
+        $this->containerExecMysql('mysql-start-master-clones', <<<EOS
             insert into testdb.test values 
             (1,'value_1'),
             (2,'value_2');
 EOS);
 
-        $this->serviceCloneService->start('mysql', '01', 1);
-        $this->containerExecMysql('mysql_01', <<<EOS
+        $this->serviceCloneService->start('mysql_start_master_clones', '01', 1);
+        $this->containerExecMysql('mysql-start-master-clones_01', <<<EOS
             insert into testdb.test values 
             (3,'value_3'),
             (4,'value_4');
 EOS);
-        $this->serviceCloneService->start('mysql', '02', 2);
+        $this->serviceCloneService->start('mysql_start_master_clones', '02', 2);
 
-        $this->containerExecMysql('mysql', <<<EOS
+        $this->containerExecMysql('mysql-start-master-clones', <<<EOS
             insert into testdb.test values 
             (3,'value_3');
 EOS);
-        $this->containerExecMysql('mysql_02', <<<EOS
+        $this->containerExecMysql('mysql-start-master-clones_02', <<<EOS
             insert into testdb.test values 
             (5,'value_5');
 EOS);
@@ -189,7 +190,7 @@ EOS);
             "1\tvalue_1",
             "2\tvalue_2",
           "3\tvalue_3",
-        ], explode("\n", $this->containerExecMysql('mysql', 'select * from testdb.test;')));
+        ], explode("\n", $this->containerExecMysql('mysql-start-master-clones', 'select * from testdb.test;')));
 
         self::assertSame([
             "idx\tval",
@@ -197,14 +198,14 @@ EOS);
             "2\tvalue_2",
             "3\tvalue_3",
             "4\tvalue_4",
-        ], explode("\n", $this->containerExecMysql('mysql_01', 'select * from testdb.test;')));
+        ], explode("\n", $this->containerExecMysql('mysql-start-master-clones_01', 'select * from testdb.test;')));
 
         self::assertSame([
             "idx\tval",
             "1\tvalue_1",
             "2\tvalue_2",
             "5\tvalue_5",
-        ], explode("\n", $this->containerExecMysql('mysql_02', 'select * from testdb.test;')));
+        ], explode("\n", $this->containerExecMysql('mysql-start-master-clones_02', 'select * from testdb.test;')));
     }
 
     /**
