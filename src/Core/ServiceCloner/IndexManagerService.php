528b30c4231123c48710a4afae55b845bb723774
Olivier MICHAUD <o.michaud@hsrv.fr>
2021-04-05 23:22:32 +0200

    Fix code standard on indexManager

--- src/Core/ServiceCloner/IndexManagerService.php
+++ src/Core/ServiceCloner/IndexManagerService.php
@@ -24,6 +24,7 @@ final class IndexManagerService implements IndexManagerServiceInterface
                 ++$nextAvailableIndex;
             }
         }
+
         return $nextAvailableIndex;
     }
 }
