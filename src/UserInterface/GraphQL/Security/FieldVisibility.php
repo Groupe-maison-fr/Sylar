997ef02db3ead435c8e5d72c659d21c30540f4e4
Olivier Michaud <olivier@micoli.org>
2023-08-16 02:24:56 +0200

    Add an environment variable to activate security

--- src/UserInterface/GraphQL/Security/FieldVisibility.php
+++ src/UserInterface/GraphQL/Security/FieldVisibility.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\UserInterface\GraphQL\Security;
 
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 final readonly class FieldVisibility
 {
     public function __construct(
+        #[Autowire('%security_enabled%')]
+        private bool $securityEnabled,
         private Security $security,
     ) {
     }
@@ -18,6 +21,9 @@ final readonly class FieldVisibility
      */
     public function emptyOnAnyRole(mixed $value, array $roles, mixed $emptyValue = null): mixed
     {
+        if (!$this->securityEnabled) {
+            return $value;
+        }
         foreach ($roles as $role) {
             if ($this->security->isGranted($role, $this->security->getUser())) {
                 return $value;
